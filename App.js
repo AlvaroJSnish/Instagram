92fc1fbca9a1504863a57047d557861b9e9a2144
AlvaroJSnish <39116439+AlvaroJSnish@users.noreply.github.com>
2018-08-20 15:27:38 +0200

    Update App.js

--- App.js
+++ App.js
@@ -6,12 +6,12 @@ import Main from './app/Main';
 export default class App extends Component {
   componentWillMount() {
     const config = {
-      apiKey: 'AIzaSyD2RBlAfBaTGBjAToZk3y8xBkQe8qxzimE',
-      authDomain: 'instagram-alvaro.firebaseapp.com',
-      databaseURL: 'https://instagram-alvaro.firebaseio.com',
-      projectId: 'instagram-alvaro',
-      storageBucket: 'instagram-alvaro.appspot.com',
-      messagingSenderId: '908451820998'
+      apiKey: '',
+      authDomain: '',
+      databaseURL: '',
+      projectId: '',
+      storageBucket: '',
+      messagingSenderId: ''
     };
     firebase.initializeApp(config);
   }
